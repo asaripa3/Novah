ce6dab48fbc0ad1e9f909c6b8f21961879369070
Adarsh <143021086+asaripa3@users.noreply.github.com>
2025-05-31 15:49:44 -0700

    v8.1
    EC2 deployment

--- web/app.py
+++ web/app.py
@@ -370,4 +370,4 @@ def add_profile_word():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+    app.run(host='0.0.0.0', port=5000)
